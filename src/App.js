4d31ae4561b699241a79e5ad3a539226fa3a1177
notefox <kitsu-geissler@t-online.de>
2023-03-09 09:38:31 +0000

    <feat>: add background panning and an up-to-date clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import mountains1 from './assets/mountain_image.jpg'
+import mountains2 from './assets/trace-hudson.jpg'
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let backgroundImages = [mountains1, mountains2]
+
+    const [currentTime, setCurrentTime] = useState('');
+
+    const [loopStarted, loopState] = useState(false);
+
+    function renderBackground(image, opacity) {
+        return <div className={"animate-backgroundPanning absolute transition-opacity duration-1000 bg-no-repeat window-element"}
+                    style={{
+                        backgroundImage: "url(" + image + ")",
+                        opacity: opacity + "%"
+        }}/>
+    }
+
+    function updateClock() {
+        setTimeout(() => {
+            let currentTime = new Date()
+            let currentTimeString = currentTime.toTimeString()
+            setCurrentTime(currentTimeString)
+            requestAnimationFrame(() => {
+                updateClock()
+            })
+        }, 500)
+    }
+
+    if(!loopStarted) {
+        loopState(true)
+        //circlingBackground()
+        updateClock()
+    }
+
+    return (
+        <div className={"App center-inner full-screen bg-secondary"}>
+            <div className={"window-element center-inner-bottom z-10"}>
+                <h1 className={"content w-full pb-8 pt-32 rounded-2xl subpixel-antialiased tracking-widest bg-gradient-to-t from-gray-600 to-transparent"}>
+                    {currentTime}
+                </h1>
+            </div>
+            {renderBackground(backgroundImages[0], 100)}
+            {/*renderBackground(backgroundImages[1], nextBackgroundOpacity)*/}
+        </div>
+    );
 }
 
 export default App;
